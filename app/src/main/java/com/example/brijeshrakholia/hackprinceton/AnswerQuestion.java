0e50c14a828679469c220a7533521027f6849960
brijeshr <brijesh.rakholia07@gmail.com>
2015-04-12 03:01:47 -0400

    POST WORKS

--- app/src/main/java/com/example/brijeshrakholia/hackprinceton/AnswerQuestion.java
+++ app/src/main/java/com/example/brijeshrakholia/hackprinceton/AnswerQuestion.java
@@ -1,36 +1,41 @@
 package com.example.brijeshrakholia.hackprinceton;
 
-import android.content.Intent;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
+import android.provider.Settings.Secure;
+
+public class AnswerQuestion extends ActionBarActivity {
 
 
-public class Home extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-    }
-    /**
-    Called when the user touches the button @+id/Ask
-     */
-    public void callAskQ(View view){
-        Intent intent = new Intent(this,AskQuestionActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_answer_question);
     }
 
-    public void callAnswerQuestion(View view){
-        Intent intent = new Intent(this,AnswerQuestion.class);
-        startActivity(intent);
-    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
+        getMenuInflater().inflate(R.menu.menu_answer_question, menu);
         return true;
     }
 
@@ -48,4 +53,11 @@ public class Home extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void updateQuestions(View view){
+
+
+    }
 }
+
+
--- app/src/main/java/com/example/brijeshrakholia/hackprinceton/AskQuestionActivity.java
+++ app/src/main/java/com/example/brijeshrakholia/hackprinceton/AskQuestionActivity.java
@@ -1,12 +1,38 @@
 package com.example.brijeshrakholia.hackprinceton;
 
+import android.content.Context;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.JsonWriter;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.UUID;
+
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 
 public class AskQuestionActivity extends ActionBarActivity {
+    private double longitude;
+    private double latitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,4 +64,92 @@ public class AskQuestionActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    public void sendQuestion(View view) throws IOException, JSONException {
+        // Retrieve latitude and longitude
+
+        TelephonyManager TM = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+        // IMEI No.
+        String imeiNo = TM.getDeviceId();
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        EditText mEdit = (EditText) findViewById(R.id.question);
+        Log.v("Longitude", Double.toString(longitude));
+
+
+        JSONObject j = new JSONObject();
+        j.put("longitude", longitude);
+        j.put("latitude", latitude);
+        j.put("android_id", imeiNo );
+        j.put("question", mEdit.getText());
+        Log.v("JSON", j.toString());
+        if (longitude != 0 && latitude != 0){
+            AsyncTest backendThread = new AsyncTest(j);
+            backendThread.execute();
+        }
+
+    }
+
+    private  class TestMain {
+        OkHttpClient client = new OkHttpClient();
+
+        // post request code here
+
+        public final MediaType JSON
+                = MediaType.parse("application/json; charset=utf-8");
+
+
+        String doPostRequest(String url, String json) throws IOException {
+            RequestBody body = RequestBody.create(JSON, json);
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .build();
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        }
+    }
+    private class AsyncTest extends AsyncTask<Void, Integer, String>{
+        private JSONObject j;
+        public AsyncTest(JSONObject json){
+            j = json;
+
+        }
+        @Override
+        protected String doInBackground(Void... params) {
+            TestMain post = new TestMain();
+            String postrequest = null;
+            try {
+                postrequest = post.doPostRequest("http://hispy.herokuapp.com/sendprompt", j.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Log.v("PostRequest: ", postrequest);
+            return postrequest;
+        }
+    }
+
 }
