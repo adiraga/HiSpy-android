46abb396b39fa1f73535f2f879df0e91a35b2a3c
brijeshr <brijesh.rakholia07@gmail.com>
2015-04-11 22:16:00 -0400

    adding http requests

--- app/src/main/java/com/example/brijeshrakholia/hackprinceton/Home.java
+++ app/src/main/java/com/example/brijeshrakholia/hackprinceton/Home.java
@@ -1,9 +1,25 @@
 package com.example.brijeshrakholia.hackprinceton;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class Home extends ActionBarActivity {
@@ -11,6 +27,7 @@ public class Home extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Answer Questions");
         setContentView(R.layout.activity_home);
     }
 
@@ -35,5 +52,36 @@ public class Home extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void updateQuestions(View view){
+
+        // Retrieve latitude and longitude
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+
+
+
+        JSONObject json = new JSONObject();
+        //Creating HTTP Request
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpResponse response;
+
+        // Creating HTTP Get and Post
+        HttpGet request = new HttpGet("http://hispy.herokuapp.com/getprompts");
+        HttpPost post = new HttpPost("http://hispy.herokuapp.com/getprompts");
+        try {
+            json.put("latitude", latitude);
+            json.put("longitude", longitude);
+            StringEntity se = new StringEntity(json.toString());
+            se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE,"application/json"));
+            post.setEntity(se);
+            response = httpclient.execute(post);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
